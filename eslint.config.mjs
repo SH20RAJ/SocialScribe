c13a9d6ea1741228997a34315f8a794ed55f7d0b
SH20RAJ <shaswatraj3@gmail.com>
2025-07-08 11:13:07 +0530

    feat: update ESLint configuration to disable specific rules for better development experience

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific rules here
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-img-element": "off",
+      "react/no-unescaped-entities": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "off",
+      "no-console": "warn",
+    },
+  }
 ];
 
 export default eslintConfig;
